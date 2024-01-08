9a23ef62664b1d7057111fff062846b557c37339
romarioscbl <romario.sarmiento@buildinglink.com>
2024-01-08 03:16:19 -0500

    Init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export default () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Games</a>
+                </li>
+                <li>
+                    <a href="">Top scores</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
